546cd6b145954844b5a45c6a880e21ffe0f92afa
naritotakizawa <toritoritorina@gmail.com>
2017-05-25 23:11:20 +0900

    1.png　などの画像ファイルクリック時は、画像を表示するビューで処理

--- dteditor2/views.py
+++ dteditor2/views.py
@@ -1,4 +1,9 @@
+import base64
+import os
+
+from django.http import Http404
 from django.shortcuts import render
+from django.views import generic
 
 from .utils import editor
 
@@ -10,3 +15,24 @@ def home(request):
         'editor': editor,
     }
     return render(request, 'dteditor2/home.html', context)
+
+
+class ImgView(generic.TemplateView):
+    """/img 画像ファイルクリックで呼び出されるビュー."""
+
+    template_name = 'dteditor2/img.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        img_path = self.kwargs['path']
+        try:
+            src = open(img_path, 'rb').read()
+        except FileNotFoundError:
+            raise Http404('1.png Not Found')
+        else:
+            extension = img_path.split('.')[-1]
+            context['img_src'] = base64.b64encode(src)
+            context['img_path'] = img_path
+            context['extension'] = extension
+            return context
+        
